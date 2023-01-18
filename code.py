573f95ff065a6ec1b71dbca27fd1ed693571fac7
Kevin Kannammalil <kevinrk29@hotmail.com>
2023-01-18 02:07:24 +0000

    Testing Git Commit

--- code.py
+++ code.py
@@ -1,103 +1,110 @@
 import discord
 from discord.ext import commands
 import asyncio
-from itertools import cycle 
+from itertools import cycle
 
+TOKEN = ''  #token value hidden to keep the bot's info hidden
 
-TOKEN = ''                               #token value hidden to keep the bot's info hidden
-
-status = ["Sleeping", "Eating", "Gaming", "Anime Time", "Eating"]
-bot = commands.Bot(command_prefix = '.')
+status = ["Sleeping", "Eating", "Gaming", "Anime Time", "Eating", "Snoring"]
+bot = commands.Bot(command_prefix='.')
 bot.remove_command("help")
+
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game = discord.Game(name="Maintenance"))
-    print("MyBot, ready for action")
+  await bot.change_presence(game=discord.Game(name="Maintenance"))
+  print("MyBot, ready for action")
+
 
 async def change_status():
-    await bot.wait_until_ready()
-    msgs = cycle(status)
+  await bot.wait_until_ready()
+  msgs = cycle(status)
 
-    while not bot.is_closed:
-        current_status = next(msgs)
-        await bot.change_presence(game = discord.Game(name = current_status))
-        await asyncio.sleep(200)  
+  while not bot.is_closed:
+    current_status = next(msgs)
+    await bot.change_presence(game=discord.Game(name=current_status))
+    await asyncio.sleep(200)
 
-@bot.event 
+
+@bot.event
 async def on_message(message):
-    print ("A user has sent a message")
-    await bot.process_commands(message)
+  print("A user has sent a message")
+  await bot.process_commands(message)
+
 
 @bot.event
 async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name="Test Role")
-    await bot.add_roles(member, role)
+  role = discord.utils.get(member.server.roles, name="Test Role")
+  await bot.add_roles(member, role)
+
 
 @bot.event
 async def on_message(message):
-    msgContent = message.content.lower()
-    if "yeet" in msgContent:
-        await bot.send_message(message.channel, "Yah!")
-    await bot.process_commands(message)
+  msgContent = message.content.lower()
+  if "yeet" in msgContent:
+    await bot.send_message(message.channel, "Yah!")
+  await bot.process_commands(message)
+
 
 @bot.event
 async def on_message(message):
-    msgContent = message.content.lower()
-    if "yah" in msgContent:
-        await bot.send_message(message.channel, "Yeet!")
-    await bot.process_commands(message)
+  msgContent = message.content.lower()
+  if "yah" in msgContent:
+    await bot.send_message(message.channel, "Yeet!")
+  await bot.process_commands(message)
+
 
 @bot.command()
 async def echo(*args):
-    output = " "
-    for words in args:
-        output += words
-        output += " "
-    await bot.say(output)
+  output = " "
+  for words in args:
+    output += words
+    output += " "
+  await bot.say(output)
+
 
 @bot.command(pass_context=True)
-async def clear(ctx, amount = 100):
-    channel = ctx.message.channel
-    messages = []
-    async for message in bot.logs_from(channel, limit = int(amount)):
-        messages.append(message)
-    await bot.delete_messages(messages)
-    await bot.say("Messages deleted")
+async def clear(ctx, amount=100):
+  channel = ctx.message.channel
+  messages = []
+  async for message in bot.logs_from(channel, limit=int(amount)):
+    messages.append(message)
+  await bot.delete_messages(messages)
+  await bot.say("Messages deleted")
+
 
 @bot.command()
 async def displayembed():
-    myEmbed = discord.Embed(
-        title = "Title",
-        description = "This is a description",
-        colour = discord.Colour.red()
-    )
-    
-    myEmbed.set_footer(text="This is a footer")
-    myEmbed.set_image(url="https://discordapp.com/assets/94db9c3c1eba8a38a1fcf4f223294185.png")
-    myEmbed.set_thumbnail(url="https://discordapp.com/assets/f72fbed55baa5642d5a0348bab7d7226.png")
-    myEmbed.set_author(name="Prodigy",
-    icon_url="https://discordapp.com/assets/e05ead6e6ebc08df9291738d0aa6986d.png")
-    myEmbed.add_field(name="Field Name", value="Field Value", inline=False)
-    myEmbed.add_field(name="Field Name", value="Field Value", inline=True)
-    myEmbed.add_field(name="Field Name", value="Field Value", inline=True)
-    await bot.say(embed=myEmbed)  
+  myEmbed = discord.Embed(title="Title",
+                          description="This is a description",
+                          colour=discord.Colour.red())
+
+  myEmbed.set_footer(text="This is a footer")
+  myEmbed.set_image(
+    url="https://discordapp.com/assets/94db9c3c1eba8a38a1fcf4f223294185.png")
+  myEmbed.set_thumbnail(
+    url="https://discordapp.com/assets/f72fbed55baa5642d5a0348bab7d7226.png")
+  myEmbed.set_author(
+    name="Prodigy",
+    icon_url=
+    "https://discordapp.com/assets/e05ead6e6ebc08df9291738d0aa6986d.png")
+  myEmbed.add_field(name="Field Name", value="Field Value", inline=False)
+  myEmbed.add_field(name="Field Name", value="Field Value", inline=True)
+  myEmbed.add_field(name="Field Name", value="Field Value", inline=True)
+  await bot.say(embed=myEmbed)
+
 
 @bot.command(pass_context=True)
 async def help(ctx):
-    author = ctx.message.author
+  author = ctx.message.author
 
-    helpEmbed = discord.Embed(
-        colour = discord.Colour.orange()
-    )
+  helpEmbed = discord.Embed(colour=discord.Colour.orange())
 
-    helpEmbed.set_author(name="help")
-    helpEmbed.set_field(
-        name=".yeet", value= "Returns Yah!", inline=False
-    )
+  helpEmbed.set_author(name="help")
+  helpEmbed.set_field(name=".yeet", value="Returns Yah!", inline=False)
 
-    await bot.send_message(author, embed=helpEmbed)
+  await bot.send_message(author, embed=helpEmbed)
+
 
 bot.loop.create_task(change_status())
 bot.run(TOKEN)
-
